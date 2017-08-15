6f6b44ddc795e245d0f5fef0a12e9bfbd5c3350d
Stone <generalms@hotmail.com>
2017-08-15 18:25:50 -0500

    Implemented pickle and merge of dataset

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -86,7 +86,7 @@ def load_class(folder):
 			image_data = Image.open(image_file) #opens image
 			image_data = image_data.convert('L') #converts to grayscale
 			image_data = image_data.resize((image_size,image_size)) #resize image to 96x96
-			imarray = np.array(image_data,dtype='float32')
+			imarray = np.array(image_data,dtype=np.float32)
 			dataset[num_images,:,:] = imarray
 			num_images += 1
 		except IOError as e:
@@ -96,6 +96,110 @@ def load_class(folder):
 	print('Shape',dataset.shape)
 	return dataset
 
-dataset = load_class(data_folders[0])
-plt.imshow(dataset[0])
-plt.show()
+def mabye_pickle(folders,force=False):
+	dataset_names = []
+	for folder in folders:
+		filename = folder + '.pickle'
+		dataset_names.append(filename)
+		if os.path.exists(filename) and not force:
+			print('%s already present. Skipping pickling' %filename)
+		else:
+			print('Pickling %s' %filename)
+			dataset = load_class(folder)
+			try:
+				with open(filename,'wb') as f:
+					pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+			except:
+				print('Unable to pickle file')
+	return dataset_names
+
+
+
+
+
+#dataset = load_class(data_folders[0])
+#plt.imshow(dataset[0])
+#plt.show()
+dataset_names = mabye_pickle(data_folders)
+
+train_size = 10
+valid_size = 5
+test_size = 5
+
+def make_arrays(num_rows,image_size):
+	if num_rows:
+		dataset = np.ndarray((num_rows,image_size,image_size),dtype=np.float32)
+		labels = np.ndarray(num_rows, dtype=np.int32)
+	else:
+		dataset,labels = None,None
+	return dataset,labels
+
+def merge_datasets(pickle_files,train_size,valid_size,test_size):
+	num_classes = len(pickle_files)
+
+	train_dataset,train_labels = make_arrays(num_classes * train_size,image_size)
+	valid_dataset,valid_labels = make_arrays(num_classes * valid_size,image_size)
+	test_dataset,test_labels = make_arrays(num_classes * test_size,image_size)
+
+	train_pos,valid_pos,test_pos = 0,0,0
+	for label, pickle_file in enumerate(pickle_files):
+		try:
+			with open(pickle_file, 'rb') as f:
+				class_set = pickle.load(f)
+				np.random.shuffle(class_set)
+				train_dataset[train_pos:train_pos+train_size,:,:] = class_set[:train_size,:,:]
+				valid_dataset[valid_pos:valid_pos+valid_size,:,:] = class_set[train_size:train_size+valid_size,:,:]
+				test_dataset[test_pos:test_pos+test_size,:,:] = class_set[train_size+valid_size:train_size+valid_size+test_size,:,:]
+				train_labels[train_pos:train_pos+train_size] = label
+				valid_labels[valid_pos:valid_pos+valid_size] = label
+				test_labels[test_pos:test_pos+test_size] = label
+
+				train_pos += train_size
+				valid_pos += valid_size
+				test_pos += test_size
+		except Exception as e:
+			print('Unable to process data:',e)
+	return train_dataset,train_labels,valid_dataset,valid_labels,test_dataset,test_labels
+
+
+train_dataset,train_labels,valid_dataset,valid_labels,test_dataset,test_labels = merge_datasets(dataset_names,train_size,valid_size,test_size)
+
+def randomize(dataset,labels):
+	permutation = np.random.permutation(labels.shape[0])
+	shuffled_dataset = dataset[permutation,:,:]
+	shuffled_labels = labels[permutation]
+	return shuffled_dataset, shuffled_labels
+
+train_dataset,train_labels = randomize(train_dataset,train_labels)
+valid_dataset,valid_size = randomize(valid_dataset,valid_labels)
+test_dataset,test_size = randomize(test_dataset,test_labels)
+
+print('Training:', train_dataset.shape,train_labels.shape)
+print('Validation', valid_dataset.shape,valid_labels.shape)
+print('Testing', test_dataset.shape,test_labels.shape)
+
+pickle_file = 'meterData.pickle'
+try:
+	f = open(pickle_file, 'wb')
+	save = {
+	     'train_dataset': train_dataset,
+	     'train_labels': train_labels,
+	     'valid_dataset': valid_dataset,
+	     'valid_labels': valid_labels,
+	     'test_dataset': test_dataset,
+	     'test_labels': test_labels,
+	}
+	pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+	f.close()
+except Exception as e:
+	print('Unable to save data to',pickle_file,':',e)
+
+statinfo = os.stat(pickle_file)
+print('Compressed pcikle size:', statinfo.st_size)
+
+
+
+
+
+
+
