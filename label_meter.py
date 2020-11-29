90f56dfaf175c4ab4686f25afd25a19e9add1527
Matthew Stone <generalms16@gmail.com>
2020-11-29 00:16:31 -0500

    cleaned up

--- label_meter.py
+++ label_meter.py
@@ -1,5 +1,3 @@
-#from app.models import DigitalMeterModel
-#from app.models import AnalogMeterModel
 import cv2
 import numpy as np
 import os
@@ -9,18 +7,7 @@ from matplotlib import pyplot as plt
 import math
 from final_model import ElectricMeterModel
 
-
-#matplotlib.use("TkAgg")
-
-
-
-
-# digital_model = DigitalMeterModel('digital_meter_model','/nas_33705.pb',
-# 											'/labelmap.pbtxt')
-
-# analog_model = AnalogMeterModel('analog_meter_model', '/analog_faster_rcnn.pb', 
-# 		'/analog_label_map.pbtxt')
-electric_model = ElectricMeterModel('meter_model', '/frozen_inference_graph.pb', 
+electric_model = ElectricMeterModel('meter_model', '/saved_model.pb', 
  		'/labelmap.pbtxt')
 
 
@@ -31,7 +18,6 @@ TEST_IMAGE_PATHS = [os.path.join(PATH_TO_TEST_IMAGES_DIR, file)
 
 
 df = pd.read_csv('analog_readings2.csv')
-# df = df.replace(np.nan, 0)
 l = df['label'].to_list()
 files = df['Name'].to_list()
 l = [int(i) for i in l]
@@ -43,27 +29,13 @@ num_match = 0
 exact_match = 0
 img_count=0
 exception_match = 0
-#print(TEST_IMAGE_PATHS)
 print(files)
 for index,img in enumerate(files):
-	#print(img)
 	cv_img = cv2.imread('test_analog/{}'.format(img),1)
-	# # #print(cv_img)
-	# plt.imshow(cv_img)
-	# plt.show()
-	# label_reading = input('Enter reading')
-	#label_readings.append('')
 	reading_str = ''
-	# print(img)
-	# #readings = digital_model.predict_reading(cv_img)
 	if(l[index]==-1):
 		continue
-	#readings = analog_model.predict_reading(cv_img)
 	meter_type, reading_str, id_number, result, exception = electric_model.predict(cv_img)
-	#print(readings)
-	# for num_dict in readings:
-	# 	reading_str+=num_dict['number']
-	# print(l[index], reading_str)
 	img_count += 1
 	if reading_str == '':
 		reading_str = '0'
@@ -76,22 +48,10 @@ for index,img in enumerate(files):
 	
 	if(l[index] == int(reading_str)):
 		exact_match+=1
-	#print(num_match)
-# 	#readings_lst.append(reading_str)
+
 	print('{} / {} images'.format(index, len(TEST_IMAGE_PATHS)))
 
-# # # lst = [i for i in range(len(file_names))]
-# # #print('File names',file_names)
 print('Accuracy {}'.format((float(num_match)/img_count)))
 print('True Accuracy {}'.format((float(exact_match)/img_count)))
 print('Number of False Positive {}'.format(float(exception_match)/(img_count - num_match)))
-
-# print(file_names)
-# print(readings_lst)
-# df = pd.DataFrame(list(zip(file_names, readings_lst, label_readings)),
-#                columns =['Name', 'val', 'label'])
-# file_names = os.listdir(PATH_TO_TEST_IMAGES_DIR)
-# df = pd.DataFrame(list(zip(file_names, label_readings)),
-#                columns =['Name', 'label'])
-# df.to_csv('analog_readings.csv')
 
